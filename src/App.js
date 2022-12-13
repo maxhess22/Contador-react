01afc892235864eb30488bf8fa2767d05a776571
maxhess22 <ma.hessalvarez@duocuc.cl>
2022-12-12 21:58:55 -0400

    ajustes de estilo

--- src/App.js
+++ src/App.js
@@ -9,14 +9,13 @@ function App() {
   }
   return (
     <div className="App">
-      <h1>Counter clicks</h1>
+      <h1 className="Title">Counter clicks</h1>
       <Contador clase="contador" valor={valor} />
       <div className="botones">
-        <Boton nombre="sumar" metodo={()=>setValor(valor +1)}></Boton>
-        <Boton nombre="restar"metodo={()=>setValor(valor-1)} ></Boton>
+        <Boton nombre="Sumar" metodo={()=>setValor(valor +1)}></Boton>
+        <Boton nombre="Restar"metodo={()=>setValor(valor-1)} ></Boton>
         <Boton nombre="Borrar" borrar={true} metodo={()=>setValor(0)} ></Boton>
       </div>
-
     </div>
   );
 }
